50095eaedf7a05486340de96f740c087d6a2b195
Muhammad AL-Muzahid <almuzahid16@cse.pstu.ac.bd>
2025-12-25 20:39:04 +0600

    Add standard CP template

---
+++ Templates/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+const double PI = acos(-1);
+const double EPS = 1e-9;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int LEN  = 2e6 + 3;
+
+
+void idea() {
+
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    // cin >> T;
+
+    for(int C = 1; C <= T; C++) {
+        // cout << "Case " << C << ": " << '\n';
+        idea();
+    }
+    return 0;
+}
